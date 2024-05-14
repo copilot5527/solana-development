dd9a11b111b5f89865da1a030e1f1a8bd3c58b8c
Venus <copilot5527@outlook.com>
2024-05-14 09:01:57 -0700

    added craet-token-mint

---
+++ client/src/wallet.ts
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+import { FC, ReactNode, useMemo } from "react";
+import { ConnectionProvider, WalletProvider, } from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
+
+const Home: NextPage = (props) => {
+  const endpoint = web3.clusterApiUrl("devnet");
+  const wallets = useMemo(() => [], []);
+  return (
+    ConnectionProvider({endpoint:endpoint})
+  )
+  // return (
+  //   <ConnectionProvider endpoint={endpoint}>
+  //     <WalletProvider wallets={wallets}>
+  //       <p>Put the rest of your app here</p>
+  //     </WalletProvider>
+  //   </ConnectionProvider>
+  // );
+};
